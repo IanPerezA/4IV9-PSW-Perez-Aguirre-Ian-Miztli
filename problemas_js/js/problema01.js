140d1fca696d458ed7a0aad535e16ca4b619564f
IanPerezA <ipereza1702@alumno.ipn.mx>
2021-04-02 17:20:55 -0600

    problema01 js

---
+++ problemas_js/js/problema01.js
@@ -0,0 +1,23 @@
+/*vamos a crear un pragama que con una funcion regular
+se encargue de validar unocamente numeros
+*/
+function validarn(e){
+    var teclado = (document.all)?e.keycode:e.which;
+    if(teclado==8)return true;
+
+    var patron= /[0-9\d .]/;
+    var prueba= String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+function CalcuInteres(){
+    var valor= document.formulario.cantidad.value;
+    var result= parseInt(valor);
+    var interes= result * 0.02;
+    var total = result+interes;
+    document.formulario.cantidadi.value="$"+total;
+}
+function borrarI()
+{
+    document.formulario.cantidadi.value="";
+    document.formulario.cantidad.value="";
+}
